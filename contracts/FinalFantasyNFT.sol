74e8733ea246adbc6fa0c8321f8c7936403137d7
Ryan Harris <harrisryan1@gmail.com>
2021-10-26 20:03:31 -0400

    working on meta encoding

--- contracts/FinalFantasyNFT.sol
+++ contracts/FinalFantasyNFT.sol
@@ -2,11 +2,18 @@
 
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+import "./libraries/Base64.sol";
+
 import "hardhat/console.sol";
 
-contract FinalFantasyNFT {
-  
+contract FinalFantasyNFT is ERC721 {
+
   struct Characteristics {
+    uint index;
     string name;
     string imageURI;
     uint hp;
@@ -17,7 +24,93 @@ contract FinalFantasyNFT {
     uint spellDamage;
   }
 
-  constructor() {
-    console.log("THIS IS MY GAME CONTRACT. NICE.");
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  Characteristics[] defaultCharacters;
+
+  mapping(uint256 => Characteristics) public nftHolderAttributes;
+  mapping(address => uint256) public nftHolders;
+
+  constructor(
+    string[] memory characterNames,
+    string[] memory characterImageURIs,
+    uint[] memory characterHp,
+    uint[] memory characterMp,
+    uint[] memory characterAttackDamage,
+    uint[] memory characterSpellDamage
+  ) ERC721("Final Fantasy", "FF") {
+
+    for (uint i = 0; i < characterNames.length; i += 1) {
+      defaultCharacters.push(Characteristics({
+        index: i,
+        name: characterNames[i],
+        imageURI: characterImageURIs[i],
+        hp: characterHp[i],
+        maxHp: characterHp[i],
+        mp: characterMp[i],
+        maxMp: characterMp[i],
+        attackDamage: characterAttackDamage[i],
+        spellDamage: characterSpellDamage[i]
+      }));
+
+      Characteristics memory character = defaultCharacters[i];
+      console.log("Done initializing %s w/ %s HP & %s MP", character.name, character.hp, character.mp);
+
+      _tokenIds.increment();
+    }
+  }
+
+  function mintNFT(uint _characterIndex) external {
+    uint256 newItemId = _tokenIds.current();
+
+    _safeMint(msg.sender, newItemId);
+
+    nftHolderAttributes[newItemId] = Characteristics({
+      index: _characterIndex,
+      name: defaultCharacters[_characterIndex].name,
+      imageURI: defaultCharacters[_characterIndex].imageURI,
+      hp: defaultCharacters[_characterIndex].hp,
+      maxHp: defaultCharacters[_characterIndex].maxHp,
+      mp: defaultCharacters[_characterIndex].mp,
+      maxMp: defaultCharacters[_characterIndex].maxMp,
+      attackDamage: defaultCharacters[_characterIndex].attackDamage,
+      spellDamage: defaultCharacters[_characterIndex].spellDamage
+    });
+
+    console.log("Minted NFT w/ tokenId %s and characterIndex %s", newItemId, _characterIndex);
+
+    nftHolders[msg.sender] = newItemId;
+
+    _tokenIds.increment();
+  }
+
+  function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+    Characteristics memory charAttr = nftHolderAttributes[_tokenId];
+    console.log(charAttr.name);
+    console.log(charAttr.imageURI);
+
+    string memory strHp = Strings.toString(charAttr.hp);
+    string memory strMaxHp = Strings.toString(charAttr.maxHp);
+    // string memory strMp = Strings.toString(charAttr.mp);
+    // string memory strMaxMp = Strings.toString(charAttr.maxMp);
+    string memory strAttackDamage = Strings.toString(charAttr.attackDamage);
+    // string memory strSpellDamage = Strings.toString(charAttr.spellDamage);
+
+    string memory json = Base64.encode(
+      bytes(
+        string(
+          abi.encodePacked(
+            '{"name": "', charAttr.name, ' -- ', Strings.toString(_tokenId), '"}'
+          )
+        )
+      )
+    );
+
+    string memory output = string(
+      abi.encodePacked("data:application/json;base64,", json)
+    );
+
+    return output;
   }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,10 +1,36 @@
+const names = ["Cloud", "Tifa", "Barret", "Aerith", "Vincent"];
+const imagesURIs = [
+  "https://en.wikipedia.org/wiki/Cloud_Strife#/media/File:Cloud_Strife.png",
+  "https://en.wikipedia.org/wiki/Tifa_Lockhart#/media/File:Tifa_Lockhart_art.png",
+  "https://en.wikipedia.org/wiki/Barret_Wallace#/media/File:Ff7-barret.png",
+  "https://en.wikipedia.org/wiki/Aerith_Gainsborough#/media/File:Aerith_Gainsborough.png",
+  "https://en.wikipedia.org/wiki/Vincent_Valentine#/media/File:Vincent_Valentine.png",
+];
+const hp = [85, 72, 100, 84, 88]; // out of 100
+const mp = [30, 45, 10, 35, 25]; // out of 50
+const spellDamage = [10, 20, 6, 14, 10]; // out of 25
+const attackDamage = [18, 12, 25, 16, 20]; // out of 25
+
 const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory(
     "FinalFantasyNFT"
   );
-  const gameContract = await gameContractFactory.deploy();
+  const gameContract = await gameContractFactory.deploy(
+    names,
+    imagesURIs,
+    hp,
+    mp,
+    attackDamage,
+    spellDamage
+  );
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
+
+  let txn = await gameContract.mintNFT(2);
+  await txn.wait();
+
+  let returnedTokenUri = await gameContract.tokenURI(1);
+  console.log("Token URI:", returnedTokenUri);
 };
 
 const runMain = async () => {
